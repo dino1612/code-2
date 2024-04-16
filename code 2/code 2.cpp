a2995d4f02122c840971de29a942fe6eb390d6da
huy44648 <huy44648@gmail.com>
2024-04-17 01:07:57 +0900

    visual --> github

--- code 2/code 2.cpp
+++ code 2/code 2.cpp
@@ -1,20 +1,63 @@
-﻿// code 2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-#include <iostream>
+#define INITIAL_SIZE 5
 
-int main()
-{
-    std::cout << "Hello World!\n";
+struct Stack {
+    int* items;
+    int top;
+    int size;
+};
+
+typedef struct Stack Stack;
+
+void initialize(Stack* s) {
+    s->items = (int*)malloc(INITIAL_SIZE * sizeof(int));
+    s->top = -1;
+    s->size = INITIAL_SIZE;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int isFull(Stack* s) {
+    return s->top == s->size - 1;
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+int isEmpty(Stack* s) {
+    return s->top == -1;
+}
+
+void push(Stack* s, int value) {
+    if (isFull(s)) {
+        s->size *= 2;
+        s->items = (int*)realloc(s->items, s->size * sizeof(int));
+    }
+    s->items[++s->top] = value;
+    printf("Pushed %d\n", value);
+}
+
+int pop(Stack* s) {
+    if (isEmpty(s)) {
+        printf("Stack is empty. Cannot pop\n");
+        return -1;
+    }
+    printf("Popped %d\n", s->items[s->top]);
+    return s->items[s->top--];
+}
+
+int main() {
+    srand(time(NULL));
+    Stack s;
+    initialize(&s);
+    int i;
+    for (i = 0; i < 30; i++) {
+        int random_num = rand() % 100 + 1;
+        if (random_num % 2 == 0) {
+            push(&s, random_num);
+        }
+        else {
+            pop(&s);
+        }
+    }
+    free(s.items); // Remember to free allocated memory
+    return 0;
+}
